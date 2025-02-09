3e94f0c35ce8d01994e5368e22132d79af9261e3
Koi <92094832+Koipix@users.noreply.github.com>
2025-02-10 00:40:20 +0800

    Added Attack Animation (still need fixing)

---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private Camera cam;
+    private Vector3 mousePos;
+
+    private void Awake() {
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 rotation = mousePos - transform.position;
+        float rotationZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, rotationZ);
+    }
+}
